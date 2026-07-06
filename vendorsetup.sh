b4e1f9dffc68d1fbc7f88c0c6f03d234e97a085e
daizeuz-dred <daizeuzdred@gmail.com>
2026-07-06 20:26:28 +0800

    vendorsetup: Update vendorsetup.sh
    Signed-off-by: daizeuz-dred <daizeuzdred@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,22 +1,46 @@
 echo start cloning repos
-VT=vendor/realme/spaced/spaced-vendor.mk
-if ! [ -a $VT ]; then git clone https://github.com/realme-mt6781-dev/android_vendor_realme_spaced vendor/realme/spaced
-fi
+
+# Kernel Tree
 KT=kernel/realme/mt6781/build.sh
-if ! [ -a $KT ]; then git clone https://github.com/realme-mt6781-dev/android_kernel_realme_mt6781.git kernel/realme/mt6781
+if ! [ -a $KT ]; then git clone https://github.com/crdroidandroid/android_kernel_realme_mt6781.git -b 16.0 kernel/realme/mt6781
 fi
-MTK_SEPOLICY=device/mediatek/sepolicy_vndr/SEPolicy.mk
-if ! [ -a $MTK_SEPOLICY ]; then git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr device/mediatek/sepolicy_vndr
+
+# Vendor Tree
+VT=vendor/realme/spaced/spaced-vendor.mk
+if ! [ -a $VT ]; then git clone https://github.com/daizeuz-dred/realme_mt6781_vendor vendor/realme/spaced
 fi
-MTK=hardware/mediatek/Android.bp
-if ! [ -a $MTK ]; then git clone https://github.com/LineageOS/android_hardware_mediatek hardware/mediatek
-fi
+
+# Oplus Hardware
 OPLUS=hardware/oplus/Android.bp
-if ! [ -a $OPLUS ]; then git clone https://github.com/LineageOS/android_hardware_oplus hardware/oplus
+if ! [ -a $OPLUS ]; then git clone https://github.com/LineageOS/android_hardware_oplus -b lineage-23.2 hardware/oplus
 fi
-VT-IMS=vendor/mediatek/ims/ims.mk
-if ! [ -a $VT-IMS ]; then git clone https://github.com/techyminati/android_vendor_mediatek_ims.git vendor/mediatek/ims
+
+# MediaTek Hardware
+MTK=hardware/mediatek/Android.bp
+if ! [ -a $MTK ]; then git clone https://github.com/LineageOS/android_hardware_mediatek -b lineage-23.2 hardware/mediatek
 fi
+
+# MediaTek Common IMS
+VT_IMS=vendor/mediatek/ims/ims.mk
+if ! [ -a $VT_IMS ]; then git clone https://github.com/daizeuz-dred/android-vendor-mediatek-ims vendor/mediatek/ims
+fi
+
+# MediaTek Sepolicy
+MTK_SEPOLICY=device/mediatek/sepolicy_vndr/SEPolicy.mk
+if ! [ -a $MTK_SEPOLICY ]; then git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr -b lineage-23.2 device/mediatek/sepolicy_vndr
+fi
+
+# Compat
+COMPAT=hardware/lineage/compat/Android.bp
+if ! [ -a $COMPAT ]; then git clone https://github.com/daizeuz-dred/android_hardware_lineage_compat hardware/lineage/compat
+fi
+
+# Pocket
 POCKET=packages/apps/PocketMode/pocket_mode.mk
 if ! [ -a $POCKET ]; then git clone https://github.com/realme-mt6781-dev/packages_apps_PocketMode packages/apps/PocketMode
+fi
+
+# Sign Keys
+KEYS=vendor/lineage-priv/keys/releasekey.pk8
+if ! [ -a $KEYS ]; then git clone https://github.com/daizeuz-dred/crdroid-12.11-keys-official vendor/lineage-priv/keys
 fi
